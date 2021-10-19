d65e637a639c1242aa9dd0cd8b71d3e8d29853e3
Your Name <mohamedabotyear@gmail.com>
2021-10-20 00:15:24 +0200

    SRP

---
+++ SRP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic[] shapes = 
+            {
+                new Square(5),
+                new Circle(10),
+                new Square(4)
+            };
+            var AreaComputation = new AreaProcessor(shapes);
+            #region before SRP  
+            //AreaComputation.areaFormat1();
+            //AreaComputation.areaFormat2();
+            #endregion
+            var printWithFormat = new AreaFormatter(AreaComputation);
+            printWithFormat.areaFormat1();
+            printWithFormat.areaFormat2();
+
+        }
+    }
+}
---
+++ SRP/Shapes.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    class Circle {
+        public double radius { private set; get; }
+        public Circle(float radius)
+        {
+            this.radius = radius;
+        }
+    }
+    class Square {
+        public double length { private set; get; }
+        public Square(float length)
+        {
+            this.length = length;
+        }
+    }
+    class AreaProcessor
+    {
+        dynamic[] shapes;
+        public AreaProcessor(dynamic []shapes)
+        {
+            this.shapes = shapes;
+        }
+        public double[] AreaCalculator() {
+            double[] areas = new double[shapes.Length];
+            for (int i = 0; i < shapes.Length; i++)
+            {
+                if (shapes[i] is Circle)
+                {
+                    areas[i] = Math.PI * Math.Pow(shapes[i].radius, 2);
+                }
+                else if (shapes[i] is Square) {
+                    areas[i] =  Math.Pow(shapes[i].length, 2);
+
+                }
+            }  
+            return areas;
+        }
+        //This problem will solve it with move formats function in separate class
+        public void areaFormat1() {
+            foreach (var item in AreaCalculator())
+            {
+                Console.WriteLine("Area:{0}",item);
+            }
+        }
+        public void areaFormat2() {
+            Console.WriteLine();
+            Console.Write("Areas:{");
+            foreach (var item in AreaCalculator())
+            {
+                Console.Write(item+",");
+            }
+            Console.WriteLine("}");
+
+        }
+
+    }
+    class AreaFormatter {
+        AreaProcessor area;
+        public AreaFormatter(AreaProcessor area)
+        {
+            this.area = area;
+        }
+        public void areaFormat1()
+        {
+            foreach (var item in area.AreaCalculator())
+            {
+                Console.WriteLine("Area:{0}", item);
+            }
+        }
+
+        public void areaFormat2()
+        {
+            Console.WriteLine();
+            Console.Write("Areas:{");
+            foreach (var item in area.AreaCalculator())
+            {
+                Console.Write(item + ",");
+            }
+            Console.WriteLine("}");
+
+        }
+
+    }
+}
